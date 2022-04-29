19fa8beed68f1e3a0295eb8b48bac5012750e19e
ZawadiSumba66 <rosesumba98@gmail.com>
2022-04-29 16:38:30 +0300

    Create Coffee folder to hold coffee drinks display

---
+++ src/components/coffee/Coffee.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+
+const Coffee = ({ coffee, navigation }) => {
+  return (
+    <View style={styles.coffeeItem}>
+        <TouchableOpacity>
+          <Image source={{ uri: coffee.image }} style={styles.coffeeImage} />
+        </TouchableOpacity>
+        <View>
+          <Text style={styles.coffeeLabel}>{coffee.name}</Text>
+        </View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  coffeeItem: {
+    flex: 1,
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    padding: 10,
+    margin: 5
+  },
+
+  coffeeLabel: {
+    fontWeight: "700",
+    fontSize: 15
+  },
+
+  coffeeImage: {
+    height: 100,
+    width: 100
+  }
+})
+
+export default Coffee;
