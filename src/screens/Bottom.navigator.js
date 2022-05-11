d34c957b46af406bef468a316d3e700c549e7d2f
ZawadiSumba66 <rosesumba98@gmail.com>
2022-05-11 12:44:12 +0300

    Move Bottom Navigator to screens folder

---
+++ src/screens/Bottom.navigator.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import Home from './Home.screen';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Orders } from './Orders.screen';
+import { Profile } from './Profile.screen';
+import { font, theme } from '../theme';
+import {HomeIcon, OrdersIcon, ProfileIcon} from '../components/Icons';
+import NavigationDrawerHeader from '../components/NavigationDrawerHeader';
+import CustomizeCoffee from '../components/coffee/CustomizeCoffee';
+
+const CoffeeStack = createStackNavigator();
+const BottomTabs = createBottomTabNavigator();
+
+
+const CoffeeStackScreen = () => {
+    return (
+      <CoffeeStack.Navigator>
+        <CoffeeStack.Screen 
+          name="Dashboard"
+          component={Home}
+          options={{headerShown: false}}
+        />
+        <CoffeeStack.Screen 
+          name='CustomizeCoffee'        
+          component={CustomizeCoffee}
+          options={{title: 'Customize Your Drink'}}
+        />
+      </CoffeeStack.Navigator>
+    )
+  }
+  
+
+const BottomNavigation = ({navigation}) => {
+    return (
+          <BottomTabs.Navigator
+            screenOptions={({route}) => ({
+            // tabBarShowLabel: false,
+            headerLeft: () => (
+              <NavigationDrawerHeader navigationProps={navigation} />
+            ),
+            tabBarActiveBackgroundColor: theme.colorGray,
+            tabBarActiveTintColor: theme.colorBlack,
+            tabBarInactiveTintColor: theme.colorAmber,
+            tabBarIcon: ({size, color}) => {
+              if (route.name === 'Home') {
+                return <HomeIcon size={size} color={color} />;
+              }
+              if (route.name === 'Orders') {
+                return <OrdersIcon size={size} color={color} />;
+              }
+              if (route.name === 'Profile') {
+                return <ProfileIcon size={size} color={color} />;
+              }
+              return null;
+            },
+          })}>
+          <BottomTabs.Screen
+            name="Home"
+            component={CoffeeStackScreen}
+            // options={{title: "Dashboard"}}
+          />
+          <BottomTabs.Screen
+            name="Orders"
+            component={Orders}
+            options={{title: 'My Orders'}}
+          />
+          <BottomTabs.Screen
+            name="Profile"
+            component={Profile}
+            options={{title: 'Your Profile'}}
+          />
+        </BottomTabs.Navigator>
+    );
+  };
+
+  export default BottomNavigation;
